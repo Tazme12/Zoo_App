f0a188bc74588ed6f945f27992d8d01df841db48
ConfidantSP <darrennwabuoku@gmail.com>
2025-10-02 18:00:06 +0100

    create table users

--- backend/db.sql
+++ backend/db.sql
@@ -4,3 +4,15 @@ CREATE DATABASE login_system (
     username text NOT NULL,
     password text NOT NULL
     );
+
+    CREATE TABLE users(
+        user_id int,
+        email text,
+        username text,
+        password text,
+        PRIMARY KEY (user_id),
+        FOREIGN KEY (user_id) REFERENCES login_system(id),
+        FOREIGN KEY (email) REFERENCES login_system(email),
+        FOREIGN KEY (username) REFERENCES login_system(username),
+        FOREIGN KEY (password) REFERENCES login_system(password)
+    );
