2c9505e7369902e0f6f7bc0834704a9167953efd
Shahid.M <shahid.mulla1@outlook.com>
2025-10-06 17:30:41 +0100

    updated repo files

--- backend/database.php
+++ backend/database.php
@@ -1,24 +1,3 @@
-<?php
-    
-?>
-
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "RZA_";
-$conn = "";
-
-$conn = mysqli_connect($db_server,
-                        $db_user,
-                        $db_pass,
-                        $db_name);
-    if ($conn){
-        echo "Server Connection : You are connected.";
-    }
-    else{
-        echo "Server Connection : Could not connect :C";
-    }
-
 
 <?php
    /* $db_server = "localhost";
