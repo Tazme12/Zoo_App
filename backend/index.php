fdcac98fa8145f60758c6fc238c23b5b802f4bf4
Tazme12 <camthornton.07@gmail.com>
2025-10-08 07:48:21 +0100

    started navigation bar in home page

--- backend/index.php
+++ backend/index.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Riget Zoo Adventures</title>
-    <link rel="stylesheet" href="../frontend/style.css">
+    <title>Riget Zoo Adventures</title>    
 </head>
 <body>
+    <link rel="stylesheet" href="../frontend/style.css">
     
     <h1>Riget Zoo Adventures</h1>
 
     <nav class="navbar">
         <ul>
+            <li><a href="index.php">Home</a></li>
             <li><a href="register.php">Register</a></li>
+            <li><a href="">About</a></li>
         </ul>
     </nav>
 
