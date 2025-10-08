aee520df0b0f9c665c1639ba03272b726b2c0651
Tazme12 <camthornton.07@gmail.com>
2025-10-08 07:40:39 +0100

    began home page

---
+++ backend/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riget Zoo Adventures</title>
+    <link rel="stylesheet" href="../frontend/style.css">
+</head>
+<body>
+    
+    <h1>Riget Zoo Adventures</h1>
+
+    <nav class="navbar">
+        <ul>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
